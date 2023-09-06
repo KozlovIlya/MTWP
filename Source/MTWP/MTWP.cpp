5ca8c7b0324845b3648b552082367583c6d6d8cd
ilya.kozlov <ilya.kozlov@thetoadman.com>
2023-09-06 02:13:25 +0200

    init

---
+++ Source/MTWP/MTWP.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MTWP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MTWP, "MTWP" );
+ 
---
+++ Source/MTWP/MTWPGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MTWPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMTWPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMTWPGameMode();
+};
+
+
+
